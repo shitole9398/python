df333b05c52ee0d7b218bf0e1cfbf563937e809a
shitole9398 <akshayshitole0919@gmail.com>
2024-09-05 14:09:45 +0000

    calc

--- Topic/CH_01_Basics/io.py
+++ Topic/CH_01_Basics/io.py
@@ -1,6 +1,29 @@
-print("hello python")
+# print("hello python")
 
-name = input("Enter Name :")
+# name = input("Enter Name :")
 
-print("Welcome",name,"!")
+# print("Welcome",name,"!")
 
+# calc --------------
+
+# num1= int(input("Enter number 1: "))
+# num2= int(input("Enter number 2: "))
+
+# print("Answer : ",(num1+num2))
+# print(type(num1))
+
+# num1= int(input("enter number 1"))
+# num2= int(input("enter number 2"))
+
+# print("answer =",(num1+num2))
+
+# num1= int(input("enter number 1"))
+# num2= int(input("enter number 2"))
+
+# print("answer =",(num1*num2))
+2
+
+# num1= int(input("enter number 1"))
+# num2= int(input("enter number 2"))
+
+# print("answer =",(num1/num2))
